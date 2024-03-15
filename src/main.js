8dbfc985018d78d5109832e34d6827c56b6dd3b3
Hung <117622033+DucHung59@users.noreply.github.com>
2024-03-15 19:32:35 +0700

    Make code prettier

--- src/main.js
+++ src/main.js
@@ -1,31 +1,36 @@
 const path = require('path')
-const express = require('express');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
 
-const route = require('./routes');
+const route = require('./routes')
 
-const app = express();
+const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.urlencoded({
-  extended: true,
-}));
-app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+app.use(express.json())
 //XML HTTP Request, fetch, axios, ...
 // app.use(morgan('combined'));
 
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+)
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views'))
 
 // Routes init
-route(app);
+route(app)
 
 // 127.0.0.1 - localhost
 app.listen(port, () => {
