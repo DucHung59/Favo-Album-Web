239eba705655016359afe8d457e00435001d431d
Hung <117622033+DucHung59@users.noreply.github.com>
2024-04-15 19:01:26 +0700

    Read Data

--- src/app/Controllers/SiteController.js
+++ src/app/Controllers/SiteController.js
@@ -5,23 +5,31 @@ const Account = require('../model/Account');
 class SiteController {
     
     async search(req, res) {
-        // res.render('search');
+        res.render('search');
         try {
             const courses = await Course.find({});
             res.json(courses);
         } catch (err) {
             res.status(400).json({ error: 'ERROR!!!' });
         }
+
+        // Course.find({})
+        //     .then(courses => res.render('search'))
+        //     .catch(err => next(err))
     }
     
-    async news(req, res) {
-        try {
-            const account = await Account.find({});
-            res.json(account);
-        } catch (err) {
-            res.status(400).json({ error: 'ERROR!!!' });
-        }
+    async news(req, res, next) {
+        // try {
+        //     const account = await Account.find({});
+        //     res.json(account);
+        // } catch (err) {
+        //     res.status(400).json({ error: 'ERROR!!!' });
+        // }
         // res.render('news');
+
+        Course.find({})
+            .then(courses => res.render('news', { courses }))
+            .catch(err => next(err))
     }
     
     async index(req, res) {
